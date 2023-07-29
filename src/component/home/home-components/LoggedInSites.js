1c542c455c8ca858cea3bea6fdc18bccd2dafe51
muhammadkibria <muhammadmridulhosenkibria>
2023-07-29 12:22:06 +0600

    data deleted successfully from chrome.storage.local

--- src/component/home/home-components/LoggedInSites.js
+++ src/component/home/home-components/LoggedInSites.js
@@ -11,23 +11,23 @@ import Grid from "@mui/material/Grid";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { Tooltip } from "@mui/material";
 import defaultLogo from "../../../assets/images/wordpress.png";
-import Modal from '@mui/material/Modal'
-import trash from "../../../assets/images/trash.png"
+import Modal from "@mui/material/Modal";
+import trash from "../../../assets/images/trash.png";
+
 const Demo = styled("div")(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
 }));
 
-
 const style = {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      width: 350,
-      bgcolor: 'background.paper',
-      boxShadow: 24,
-      p: 4,
-    };
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 350,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function LoggedInSites({ searchQuery }) {
   const [dense] = useState(false);
@@ -35,6 +35,7 @@ export default function LoggedInSites({ searchQuery }) {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
 
   useEffect(() => {
     // eslint-disable-next-line no-undef
@@ -45,12 +46,13 @@ export default function LoggedInSites({ searchQuery }) {
     });
   }, []);
 
+
+
   if (tokens === null) {
     return <div>Loading...</div>;
   }
 
   const myData = Object.values(tokens);
-
   const filteredData = myData.filter((item) => {
     const displayName = item.user_display_name
       ? item.user_display_name.toLowerCase()
@@ -62,79 +64,64 @@ export default function LoggedInSites({ searchQuery }) {
       siteUrl.includes(searchQuery.toLowerCase())
     );
   });
-
-  // function drop(key) {
-  //   // eslint-disable-next-line no-undef
-  //   chrome.storage.local.get("loginMeNowTokens", function (data) {
-  //     let tokens = data.loginMeNowTokens ? data.loginMeNowTokens : {};
-  //     delete tokens[key];
-  //     // eslint-disable-next-line no-undef
-  //     chrome.storage.local.set({ loginMeNowTokens: tokens });
-  //   });
-  // }
   
-    // filteredData.map((item, index)=> console.log(item, index));
 
-    if (Object.keys(filteredData).length > 0) {
-      for (const key in filteredData) {
-        console.log(key, filteredData[key])
-      }
-    }
+  function drop(key) {
+    // eslint-disable-next-line no-undef
+    chrome.storage.local.get("loginMeNowTokens", function (data) {
+      let tokens = data.loginMeNowTokens ? data.loginMeNowTokens : {};
+      console.log(tokens[key], tokens.key)
+      delete tokens[key]; 
+      setTokens(tokens)
+      // eslint-disable-next-line no-undef
+      chrome.storage.local.set({ loginMeNowTokens: tokens }, function () {
+        
+      });
+    });
+  }
+  
+  
+  const entries = Object.entries(tokens);
 
-    
-
+  const listItems = [];
+  for (const [key, value] of entries) {
+    listItems.push(
+      <ListItem
+        key={key}
+        secondaryAction={
+          <Tooltip title="Delete" placement="left">
+            <IconButton
+              edge="end"
+              aria-label="delete"
+              onClick={() => drop(key)}
+            >
+              <DeleteIcon className="text-[#005E54] hover:text-[#d11a2a]" />
+            </IconButton>
+          </Tooltip>
+        }
+      >
+        <ListItemAvatar>
+          <Avatar
+            src={
+              value.site_icon_url === "" ? defaultLogo : value.site_icon_url
+            }
+          />
+        </ListItemAvatar>
+        <ListItemText
+          primary={value.user_display_name}
+          secondary={value.site_url}
+        />
+      </ListItem>
+    );
+  }
 
   return (
     <>
-<div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style} className="rounded-[8px]">
-          <div className="flex justify-center"><img src={trash} alt="" /></div>
-          <h1 className="text-center text-[#000000] font-bold my-4 text-[17px]">Are you sure you want to delete your account?</h1>
-          <div className="flex justify-around">
-            <button className="bg-[#d11a2a] hover:bg-[#ac0412] text-white font-bold py-2 px-4 rounded">Yes, Delete</button>
-            <button className="bg-[#28a745] hover:bg-[#218838] text-white font-bold py-2 px-4 rounded" onClick={handleClose}>No, Cancel</button>
-          </div>
-        </Box>
-      </Modal>
-    </div>
-
-
       <Box className="w-full px-2">
         <Grid>
           <Demo>
             <List dense={dense}>
-              {filteredData.map((item) => (
-                <ListItem
-                  key={item.id}
-                  secondaryAction={
-                    <Tooltip title="Delete" placement="left">
-                      <IconButton edge="end" aria-label="delete" onClick={handleOpen}>
-                        <DeleteIcon className="text-[#005E54] hover:text-[#d11a2a]"/>
-                      </IconButton>
-                    </Tooltip>
-                  }
-                >
-                  <ListItemAvatar>
-                    <Avatar
-                      src={
-                        item.site_icon_url === ""
-                          ? defaultLogo
-                          : item.site_icon_url
-                      }
-                    />
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={item.user_display_name}
-                    secondary={item.site_url}
-                  />
-                </ListItem>
-              ))}
+              {listItems}
             </List>
           </Demo>
         </Grid>
