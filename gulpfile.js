7c9261e64eecbdd669159487a3cb6b325fb30e39
Mridul Hosen <muhammadmridulhosenkibria@gmail.com>
2023-08-27 11:21:46 +0600

    create another manifest file for firefox browser

--- gulpfile.js
+++ gulpfile.js
@@ -16,18 +16,9 @@ gulp.task('package-chrome', () => {
 
 // Build and package for Firefox
 gulp.task('build-firefox', () => {
-  return gulp.src(['build/**', '!src/chrome'])
+  return gulp.src(['build/**', '!src/chrome', '!build/manifest.json', 'firefox-manifest/manifest.json'])
     .pipe(replace('chrome.storage.local', 'browser.storage.local'))
     .pipe(replace('type="datetime-local"', 'type="date"'))
-    .pipe(replace(/"manifest_version":\s*3/g, '"manifest_version": 2'))
-    .pipe(replace('"host_permissions": ["http://*/*", "https://*/*"],', ''))
-    .pipe(replace('"update_url": "https://clients2.google.com/service/update2/crx",', '"homepage_url": "https://loginmenow.com/",'))
-    .pipe(replace(/"action": {\s*"default_popup": "index.html",\s*"default_title": "Login Me Now"\s*},/, '"browser_action": {\n    "default_icon": "icon.png",\n    "default_popup": "index.html",\n    "default_title": "Login Me Now"\n  },'))
-    .pipe(replace(/"permissions": \[\s*"storage",\s*"scripting"\s*\],/, '"permissions": [\n    "storage",\n    "scripting",\n    "webRequest",\n    "<all_urls>"\n  ],'))
-    .pipe(replace(/"content_scripts": [\s*{[^}]*}\s*]/, '[\n    {\n      "matches": ["http://*/wp-admin/*", "https://*/wp-admin/*"],\n      "js": ["scripting.js"]\n    }\n  ]'))
-    .pipe(replace(/"content_security_policy": {[^}]*},/, ''))
-
-
     .pipe(gulp.dest('firefox'));
 });
 
