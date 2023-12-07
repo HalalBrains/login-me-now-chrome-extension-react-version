b4e11f2d90ca776cd48a89885761f69961881690
muhammadkibria <muhammadmridulhosenkibria@gmail.com>
2023-12-07 15:54:16 +0600

    dragging is working temporary

--- src/component/home/home-components/LoggedInSites.js
+++ src/component/home/home-components/LoggedInSites.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import List from "@mui/material/List";
 import Grid from "@mui/material/Grid";
 import DeleteIcon from "@mui/icons-material/Delete";
 import defaultLogo from "../../../assets/images/wordpress.png";
 import Modal from "@mui/material/Modal";
 import trash from "../../../assets/images/trash.png";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import jwt from "jwt-decode";
 import { Tooltip } from "@mui/material";
 import { ToastContainer, toast } from "react-toastify";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const Demo = styled("div")(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
@@ -32,106 +32,130 @@ export default function LoggedInSites({ searchQuery }) {
   const [tokens, setTokens] = useState({});
   const [open, setOpen] = useState(false);
   const [selectedKey, setSelectedKey] = useState(null);
-  const [isDeleted, setIssDeleted] = useState(false);
+  const [isDeleted, setIsDeleted] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [paused, setPaused] = useState(false);
   const [somethingWrong, setSomethingWrong] = useState(false);
   const location = useLocation();
+  const [listItems, setListItems] = useState([]);
 
   useEffect(() => {
-    if (paused) {
-      toast.warn(`${"Extension Token Is Paused!"}`, {
-        position: "top-center",
-        autoClose: 1000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
+    // eslint-disable-next-line no-undef
+    chrome.storage.local.get("loginMeNowTokens", function (data) {
+      let tokens = data.loginMeNowTokens ? data.loginMeNowTokens : {};
+      setTokens(tokens);
+
+      const entries = Object.entries(tokens);
+      const currentDatetime = new Date();
+      const timestamp = currentDatetime.getTime() / 1000;
+      const roundedTimeStamp = Math.floor(timestamp);
+
+      const storedOrder = data.loginMeNowOrder || entries.map(([key]) => key);
+
+      const updatedListItems = storedOrder.map((key) => {
+        const value = tokens[key];
+        const decodedToken = jwt(value.token === undefined ? value : value.token);
+        const expiredDate = decodedToken.exp;
+
+        return {
+          key: key,
+          value: isLoading[key] ? (
+            <div className="bg-[#dce5f3] mb-[5px] mx-[8px] rounded-[4px]">
+              <div className="stage">
+                <div className="dot-pulse"></div>
+              </div>
+            </div>
+          ) : (
+            <div
+              draggable
+              className={
+                roundedTimeStamp >= expiredDate
+                  ? `bg-red-300 rounded-[4px] mb-[5px] flex justify-between items-center mx-[8px]`
+                  : `hover:bg-[#dce5f3] hover:rounded-[4px] mb-[5px] flex justify-between items-center mx-[8px]`
+              }
+              key={key}
+            >
+              <div
+                className={` ${
+                  roundedTimeStamp >= expiredDate
+                    ? "flex items-center w-full py-3 pl-5 pointer-events-none"
+                    : "flex items-center w-full py-3 pl-5 cursor-pointer"
+                }`}
+                onClick={() => handleLoginToWebsite(key)}
+              >
+                <img
+                  src={
+                    value.site_icon_url === "" ||
+                    value.site_icon_url === null ||
+                    value.site_icon_url === undefined ||
+                    value === "" ||
+                    value === null ||
+                    value === undefined ||
+                    value === value.token
+                      ? defaultLogo
+                      : value.site_icon_url
+                  }
+                  alt=""
+                  className="h-10 w-10 rounded-full inline-block"
+                />
+                <div className="pl-4">
+                  <h1 className="text-[16px] font-medium">
+                    {value.user_display_name === "" ||
+                    value.user_display_name === null ||
+                    value.user_display_name === undefined ||
+                    value === "" ||
+                    value === null ||
+                    value === undefined ||
+                    value === value.token
+                      ? "Empty Name"
+                      : value.user_display_name}
+                  </h1>
+                  <h6>
+                    {value.site_url === "" ||
+                    value.site_url === null ||
+                    value.site_url === undefined ||
+                    value === "" ||
+                    value === null ||
+                    value === undefined ||
+                    value === value.token
+                      ? "Empty Url"
+                      : value.site_url}
+                  </h6>
+                </div>
+              </div>
+              <Tooltip title="Delete" placement="left">
+                <div
+                  onClick={() => handleOpen(key)}
+                  className="w-[4.75rem] flex justify-center items-center h-[72px] cursor-pointer hover:bg-[#f3dcdc] rounded-[4px] group"
+                >
+                  <DeleteIcon className="text-[#005e5496] group-hover:text-[#d11a2a] transition-colors duration-300" />
+                </div>
+              </Tooltip>
+            </div>
+          ),
+        };
       });
 
-      setPaused(false);
+      setListItems(updatedListItems);
+    });
+  }, [isLoading, somethingWrong, location.state, isDeleted]);
+
+  const handleDragEnd = (result) => {
+    if (!result.destination) {
+      return;
     }
-  }, [paused]);
 
-  useEffect(() => {
-    if (somethingWrong) {
-      toast.error("Something Went Wrong!", {
-        position: "top-center",
-        autoClose: 1000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
+    const reorderedListItems = Array.from(listItems);
+    const [removed] = reorderedListItems.splice(result.source.index, 1);
+    reorderedListItems.splice(result.destination.index, 0, removed);
 
-      setSomethingWrong(false);
-    }
-  }, [somethingWrong]);
+    setListItems(reorderedListItems);
 
-  useEffect(() => {
-    if (
-      (location.state && location.state.success === true) ||
-      (location.state && location.state.tokenSuccess === true) ||
-      isDeleted
-    ) {
-      toast.success(
-        `${
-          location.state && location.state.success === true
-            ? "Saved Successfully!"
-            : isDeleted === true
-            ? "Deleted Successfully!"
-            : location.state && location.state.tokenSuccess === true
-            ? "Token Saved Successfully!"
-            : ""
-        }`,
-        {
-          position: "top-center",
-          autoClose: 1000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        }
-      );
-    }
-  }, [location.state, isDeleted]);
-
-  const handleOpen = (key) => {
-    setOpen(true);
-    setSelectedKey(key);
+    // Store the updated order in local storage
+    const updatedOrder = reorderedListItems.map((item) => item.key);
+    // eslint-disable-next-line no-undef
+    chrome.storage.local.set({ loginMeNowOrder: updatedOrder });
   };
-  const handleClose = () => setOpen(false);
-
-  useEffect(() => {
-    // eslint-disable-next-line no-undef
-    chrome.storage.local.get("loginMeNowTokens", function (data) {
-      let tokens = data.loginMeNowTokens ? data.loginMeNowTokens : {};
-      setTokens(tokens);
-    });
-  }, []);
-
-  if (tokens === null) {
-    return <div>Loading...</div>;
-  }
-
-  function drop(key) {
-    // eslint-disable-next-line no-undef
-    chrome.storage.local.get("loginMeNowTokens", function (data) {
-      let tokens = data.loginMeNowTokens ? data.loginMeNowTokens : {};
-      delete tokens[key];
-      setTokens(tokens);
-      // eslint-disable-next-line no-undef
-      chrome.storage.local.set({ loginMeNowTokens: tokens }, function () {
-        setIssDeleted(true);
-      });
-    });
-  }
 
   const handleLoginToWebsite = (key) => {
     setIsLoading((prevState) => ({ ...prevState, [key]: true }));
@@ -154,8 +178,7 @@ export default function LoggedInSites({ searchQuery }) {
         .then((result) => {
           setIsLoading((prevState) => ({ ...prevState, [key]: false }));
           if (typeof result.data.status !== "undefined") {
-            let message = `Something wen't wrong!`;
-            // eslint-disable-next-line default-case
+            let message = `Something went wrong!`;
             switch (result.data.status) {
               case "pause":
                 message = "Current token status is Paused.";
@@ -167,15 +190,22 @@ export default function LoggedInSites({ searchQuery }) {
               case "blocked":
                 message = "Current token status is Blocked.";
                 break;
-              // eslint-disable-next-line no-duplicate-case
-              case "blocked":
-                // eslint-disable-next-line no-unused-vars
-                message = "Current token status is Blocked.";
+              default:
                 break;
             }
+            toast.warn(message, {
+              position: "top-center",
+              autoClose: 1000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+            });
             return;
           }
-          // eslint-disable-next-line no-undef
+    // eslint-disable-next-line no-undef
           chrome.tabs.create({
             url: result.data.link + "&extension=chrome",
             active: false,
@@ -188,98 +218,27 @@ export default function LoggedInSites({ searchQuery }) {
     });
   };
 
-  const entries = Object.entries(tokens);
-  const currentDatetime = new Date();
-  const timestamp = currentDatetime.getTime() / 1000;
-  const roundedTimeStamp = Math.floor(timestamp);
+  const handleOpen = (key) => {
+    setOpen(true);
+    setSelectedKey(key);
+  };
 
-  const listItems = [];
-  for (const [key, value] of entries) {
-    const decodedToken = jwt(value.token === undefined ? value : value.token);
-    const expiredDate = decodedToken.exp;
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedKey(null);
+  };
 
-    if (
-      searchQuery.toLowerCase() === "" ||
-      value.user_display_name.toLowerCase().includes(searchQuery) ||
-      value.site_url.toLowerCase().includes(searchQuery)
-    ) {
-      listItems.push(
-        isLoading[key] ? (
-          <div className="bg-[#dce5f3] mb-[5px] mx-[8px] rounded-[4px]">
-            <div class="stage">
-              <div class="dot-pulse"></div>
-            </div>
-          </div>
-        ) : (
-          <div
-            className={
-              roundedTimeStamp >= expiredDate
-                ? `bg-red-300 rounded-[4px] mb-[5px] flex justify-between items-center mx-[8px]`
-                : `hover:bg-[#dce5f3] hover:rounded-[4px] mb-[5px] flex justify-between items-center mx-[8px]`
-            }
-            key={key}
-          >
-            <div
-              className={` ${
-                roundedTimeStamp >= expiredDate
-                  ? "flex items-center w-full py-3 pl-5 pointer-events-none"
-                  : "flex items-center w-full py-3 pl-5 cursor-pointer"
-              }`}
-              onClick={() => handleLoginToWebsite(key)}
-            >
-              <img
-                src={
-                  value.site_icon_url === "" ||
-                  value.site_icon_url === null ||
-                  value.site_icon_url === undefined ||
-                  value === "" ||
-                  value === null ||
-                  value === undefined ||
-                  value === value.token
-                    ? defaultLogo
-                    : value.site_icon_url
-                }
-                alt=""
-                className="h-10 w-10 rounded-full inline-block"
-              />
-              <div className="pl-4">
-                <h1 className="text-[16px] font-medium">
-                  {value.user_display_name === "" ||
-                  value.user_display_name === null ||
-                  value.user_display_name === undefined ||
-                  value === "" ||
-                  value === null ||
-                  value === undefined ||
-                  value === value.token
-                    ? "Empty Name"
-                    : value.user_display_name}
-                </h1>
-                <h6>
-                  {value.site_url === "" ||
-                  value.site_url === null ||
-                  value.site_url === undefined ||
-                  value === "" ||
-                  value === null ||
-                  value === undefined ||
-                  value === value.token
-                    ? "Empty Url"
-                    : value.site_url}
-                </h6>
-              </div>
-            </div>
-            <Tooltip title="Delete" placement="left">
-              <div
-                onClick={() => handleOpen(key)}
-                className="w-[4.75rem] flex justify-center items-center h-[72px] cursor-pointer hover:bg-[#f3dcdc] rounded-[4px] group"
-              >
-                <DeleteIcon className="text-[#005e5496] group-hover:text-[#d11a2a] transition-colors duration-300" />
-              </div>
-            </Tooltip>
-          </div>
-        )
-      );
-    }
-  }
+  const drop = (key) => {
+    // eslint-disable-next-line no-undef
+    chrome.storage.local.get("loginMeNowTokens", function (data) {
+      let tokens = data.loginMeNowTokens ? data.loginMeNowTokens : {};
+      delete tokens[key];
+    // eslint-disable-next-line no-undef
+      chrome.storage.local.set({ loginMeNowTokens: tokens }, function () {
+        setIsDeleted(true);
+      });
+    });
+  };
 
   return (
     <>
@@ -290,19 +249,32 @@ export default function LoggedInSites({ searchQuery }) {
       >
         <Grid>
           <Demo>
-            <List dense={dense}>
-              {listItems.length === 0 && (
-                <div className="text-center">
-                  <p>No Website Found</p>
-                  <Link to="/add-new-site">
-                    <button className="bg-[#005E54] hover:bg-[#005e55ef] text-white font-bold py-3 rounded w-full mt-4">
-                      Add New Site
-                    </button>
-                  </Link>
-                </div>
-              )}
-              {listItems}
-            </List>
+            <DragDropContext onDragEnd={handleDragEnd}>
+              <Droppable droppableId="droppable">
+                {(provided) => (
+                  <div {...provided.droppableProps} ref={provided.innerRef}>
+                    {listItems.map((item, index) => (
+                      <Draggable
+                        key={item.key}
+                        draggableId={item.key}
+                        index={index}
+                      >
+                        {(provided, snapshot) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            {item.value}
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
           </Demo>
         </Grid>
       </Box>
